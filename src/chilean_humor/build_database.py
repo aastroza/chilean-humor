e9bbc648f9589b58e0d644383eccd7371c4bfff7
Alonso Astroza Tagle <alonsoastroza@gmail.com>
2024-05-23 23:19:15 -0400

    first github actions workflow

---
+++ src/chilean_humor/build_database.py
@@ -0,0 +1,18 @@
+import sqlite_utils
+import pandas as pd
+
+# Create a connection to the database
+db = sqlite_utils.Database("humor.db")
+
+# Insert data from CSV files into the database
+routines_df = pd.read_csv("data/FICVN-routines.csv")
+comedians_df = pd.read_csv("data/FICVN-comedians.csv")
+shows_df = pd.read_csv("data/FICVN-shows.csv")
+
+db["routines"].insert_all(routines_df.to_dict(orient="records"), alter=True, pk="ID")
+db["comedians"].insert_all(comedians_df.to_dict(orient="records"), alter=True, pk="ID")
+db["shows"].insert_all(shows_df.to_dict(orient="records"), alter=True, pk="ID")
+
+# Add foreign keys
+db["comedians"].add_foreign_key("SHOWID", "shows", "ID")
+db["routines"].add_foreign_key("SHOWID", "shows", "ID")
